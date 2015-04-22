f540fb2171a7ec8f25157098d1c3106ea945ae08
Alex <alexcorkin@gmail.com>
2015-04-22 21:41:52 +1200

    fixed non-standard characters

--- routes/index.js
+++ routes/index.js
@@ -2,32 +2,32 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
 });
 
-router.get('/aboutUs', function(req, res, next) {
-  res.render('aboutUs', { title: 'Express' });
+router.get('/aboutUs', function (req, res, next) {
+    res.render('aboutUs', {title: 'Express'});
 });
 
-router.get('/blog', function(req, res, next) {
-  res.render('blog', { title: 'Express' });
+router.get('/blog', function (req, res, next) {
+    res.render('blog', {title: 'Express'});
 });
 
-router.get('/media', function(req, res, next) {
-  res.render('media', { title: 'Express' });
+router.get('/media', function (req, res, next) {
+    res.render('media', {title: 'Express'});
 });
 
-router.get('/portfolio', function(req, res, next) {
-  res.render('portfolio', { title: 'Express' });
+router.get('/portfolio', function (req, res, next) {
+    res.render('portfolio', {title: 'Express'});
 });
 
-router.get('/recruitment', function(req, res, next) {
-  res.render('recruitment', { title: 'Express' });
+router.get('/recruitment', function (req, res, next) {
+    res.render('recruitment', {title: 'Express'});
 });
 
-router.get('/unitStructure.ejs', function(req, res, next) {
-  res.render('unitStructure', { title: 'Express' });
+router.get('/unitStructure', function (req, res, next) {
+    res.render('unitStructure', {title: 'Express'});
 });
 
 module.exports = router;
